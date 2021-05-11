0a71214dcec193fbdf4aa11ffb0261455be026bb
Vineet <rvineet663@gmail.com>
2021-05-11 13:38:26 +0530

    basic structure

---
+++ assignment/src/components/App.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line
+import React, { useState } from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    
+    <div className="left"><h3>Course Categories </h3> </div>
+
+    <div className="right">
+    
+    <h3>Search Courses</h3>
+     <input className="input"/>
+    </div>
+     
+    </div>
+  );
+}
+
+export default App;
